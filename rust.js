877ac84a0531c5a05ed7a84501bd861e735fc47a
Eder Marques <eder.marques@citi.org.br>
2018-09-19 01:19:27 -0300

    number of loops is 10⁸, gets x number from parameter

--- rust.js
+++ rust.js
@@ -1,35 +1,39 @@
-// console.log('\033c');
+console.log('\033c');
 
 const start = new Date();
-const numSize = 2;
-const numOfLoops = 10**7;
+const numSize = process.argv[2];
+const numOfLoops = 10 ** 8;
 let maxCount = 0;
 let totalCount = 0;
 
+console.log(numSize);
 
 const findNum = () => {
   const pickedNumbers = [];
-  const randomNumbers = [];
+  const randNumbers = [];
   let count = 0;
 
   const getRandomNumber = () => {
     count++;
     const num = Math.floor((Math.random() * numSize) + 1);
-    randomNumbers.push(num);
     if (pickedNumbers.indexOf(num) < 0) pickedNumbers.push(num);
-    if (pickedNumbers.length < numSize) getRandomNumber();
+    // randNumbers.push(num);
   }
-
-  getRandomNumber();
-
+  
+  while (pickedNumbers.length < numSize) {
+    getRandomNumber();
+  }
+  
   totalCount += count;
-  if(count > maxCount) maxCount = count;
+  if (count > maxCount) maxCount = count;
+  // console.log(randNumbers);
 };
 
 for (let i = 0; i < numOfLoops; i++) {
   findNum();
 }
 
+console.log(numSize);
 console.log('Average:', totalCount / numOfLoops);
 console.log('Max Count:', maxCount);
 console.log('Time:', new Date() - start);
